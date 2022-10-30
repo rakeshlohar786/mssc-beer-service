0176cc5c19342365c9e8cccc0d4c25013b32ca33
rakeshlohar786 <rakeshsl546@gmail.com>
2022-10-30 19:09:40 +0530

    added validation to beer class. closes #8

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,13 +27,13 @@ public class BeerController {
 	}
 	
 	@PostMapping
-	public ResponseEntity saveNewBear(@RequestBody BeerDto beerDto) {
+	public ResponseEntity saveNewBear(@Validated @RequestBody BeerDto beerDto) {
 		
 		return new ResponseEntity(HttpStatus.OK);
 	}
 	
 	@PutMapping("/{beerId}")
-	public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+	public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@Validated @RequestBody BeerDto beerDto) {
 		
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
