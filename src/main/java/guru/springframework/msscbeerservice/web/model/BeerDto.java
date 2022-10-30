a3bd938a87d0fe02197ff3fb5504b1da07a0677a
rakeshlohar786 <rakeshsl546@gmail.com>
2022-10-30 18:58:28 +0530

    added validation on Beer Dto. closes #8

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,15 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,18 +24,33 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+  @Null
   private UUID id;
+  
+  @Null
   private Integer version;
   
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
   private OffsetDateTime createdDate;
+  
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
   private OffsetDateTime lastModifiedDate;
   
+  @NotBlank
   private String beerName;
   
+  @NotNull
   private BeerStyleEnum beerStyle;
   
+  @NotNull
   private Long upc;
   
+  
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+  @Positive
+  @NotNull
   private BigDecimal price;
   
   private Integer quantityOnHand;
